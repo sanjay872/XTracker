1e19c761b188e81acf62cf0c7346190bb7866351
sanjay <rockysanjay287@gmail.com>
2024-05-22 19:43:35 -0500

    chore: Refactor Home component and styles
    ```

--- components/Home.jsx
+++ components/Home.jsx
@@ -6,16 +6,20 @@ const Home = () => {
     const imageHeight = 200;
 
     return (
-        <div>
-            <h1>Welcome to Expense Tracker</h1>
+        <div className="home">
+            <div className="title">
+                <h1>Expense Tracker</h1>
+                <p>Keep track of your expenses with this simple and easy to use expense tracker.</p>
+            </div>
             <div className="grid">
-                <Image src="/asserts/images/home_1.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
-                <Image src="/asserts/images/home_2.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
-                <Image src="/asserts/images/home_3.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
-                <Image src="/asserts/images/home_4.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
-                <Image src="/asserts/images/home_5.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
-                <Image src="/asserts/images/home_6.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
-                <Image src="/asserts/images/home_7.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
+                <Image className="img item1" src="/asserts/images/home_1.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
+                <Image className="img item2" src="/asserts/images/home_2.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
+                <Image className="img item3" src="/asserts/images/home_3.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
+                <Image className="img item4" src="/asserts/images/home_4.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
+                <Image className="img item5" src="/asserts/images/home_5.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
+                <Image className="img item6" src="/asserts/images/home_6.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
+                <Image className="img item7" src="/asserts/images/home_7.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
+                <Image className="img item8" src="/asserts/images/home_8.jpg" alt="Expense Tracker" width={imageWidth} height={imageHeight} />
             </div>
         </div>
     );
